cd5bd433f8ca9bc0355a05b030e5cef5a7080ee0
hiro0218 <hiro0218@gmail.com>
2016-07-02 16:30:14 +0900

    add Archive

---
+++ templates/template-archive.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Template Name: アーカイブ
+ */
+
+global $wpdb;
+
+$sql = "SELECT DISTINCT MONTH( post_date ) AS month, YEAR( post_date ) AS year, COUNT( id ) AS count
+        FROM $wpdb->posts
+        WHERE post_status = 'publish'
+          AND post_date <= now()
+          AND post_type = 'post'
+        GROUP BY month, year
+        ORDER BY post_date DESC";
+$months = $wpdb->get_results($sql);
+?>
+<article class="entry mdl-cell mdl-cell--12-col">
+  <header>
+    <h1 class="entry-title"><?php the_title(); ?></h1>
+  </header>
+  <div class="entry-content">
+    <section>
+      <?php
+        $tmp_year = null;
+        foreach ($months as $row):
+          $year = $row->year;
+          $month = $row->month;
+
+          if ($tmp_year != $year) {
+            // ループの最初以外
+            if ($month !== reset($months)) {
+              echo '</ul>';  // 前のループのタグを閉じる
+            }
+
+            echo '<h2>'. $year .'年</h2>';
+            echo '<ul class="archive-list">';
+        }
+        ?>
+        <li>
+          <a href="<?= BLOG_URL ?><?= $year; ?>/<?= str_pad($month, 2, 0, STR_PAD_LEFT); ?>">
+            <?= str_pad($month, 2, 0, STR_PAD_LEFT); ?>月 (<?= $row->count; ?>)
+          </a>
+        </li>
+        <?php
+            // 次のループに渡す
+            $tmp_year = $year;
+
+            // ループが最後のとき
+            if ($month === end($months)) {
+              echo "</ul>"; // タグを閉じる
+            }
+
+          endforeach;
+        ?>
+    </section>
+  </div>
+</article>
