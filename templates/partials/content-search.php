c17f185354484647b30c31d19a3d65b232c7ce79
hiro <hiro0218@gmail.com>
2016-07-14 19:44:32 +0900

    fix: Home & Search result page to be same

--- templates/partials/content-search.php
+++ templates/partials/content-search.php
@@ -1,15 +1,1 @@
-<article class="mdl-cell mdl-cell--4-col card-container mdl-card mdl-color--white" title="<?php the_title(); ?>">
-  <header class="mdl-card__title">
-    <h2 class="entry-title mdl-card__title-text">
-      <a href="<?php the_permalink(); ?>">
-        <?php the_title(); ?>
-      </a>
-    </h2>
-  </header>
-  <div class="entry-summary mdl-card__supporting-text mdl-card--expand">
-    <?php the_excerpt(); ?>
-  </div>
-  <div class="mdl-card__supporting-text">
-    <?php if (get_post_type() === 'post') { get_template_part('partials/entry-meta'); } ?>
-  </div>
-</article>
+<?php get_template_part('partials/content'); ?>
