09990a259d9513a6a4cba023738b6c9b70153c01
hiro0218 <hiro0218@gmail.com>
2016-07-17 23:08:06 +0900

    fix

--- src/setup.php
+++ src/setup.php
@@ -10,7 +10,7 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main', asset_path('scripts/main.js'), [], null, true);
     if (!wp_is_mobile()) {
-        wp_enqueue_style('google/font', 'https://fonts.googleapis.com/css?family=Open+Sans:400,600|Source+Code+Pro:400,700', false, null);
+        wp_enqueue_style('google/font', 'https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700', false, null);
     }
 }, 100);
 
