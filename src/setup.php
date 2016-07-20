1eefaf2ee72207e4a3790c14fc8e81a4d2eade11
hiro <hiro0218@gmail.com>
2016-07-20 17:20:08 +0900

    fix: style

--- src/setup.php
+++ src/setup.php
@@ -80,7 +80,7 @@ add_action('widgets_init', function () {
     $config = [
         'before_widget' => '<section class="widget %1$s %2$s">',
         'after_widget'  => '</section>',
-        'before_title'  => '<h3>',
+        'before_title'  => '<h3 class="widget-title">',
         'after_title'   => '</h3>'
     ];
     register_sidebar([
