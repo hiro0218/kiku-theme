652573587d4136b0e5fc65cb4dfed804e74ebb54
hiro <hiro0218@gmail.com>
2016-07-20 11:17:37 +0900

    remove home animation

--- assets/scripts/routes/Home.js
+++ assets/scripts/routes/Home.js
@@ -1,12 +1,5 @@
-import common from '../module/common.js';
-import animation from '../module/animation.js';
-
 export default {
   init() {
-    if ( !common.isSmartPhone() ) {
-      var article = document.getElementsByTagName('article');
-      animation.show(article, 'showIn');
-    }
   },
   finalize() {
 
