49281ead1800852a3452514ee76a020f050b5a55
hiro <hiro0218@gmail.com>
2016-09-02 19:11:19 +0900

    fix: archive page

--- templates/template-archive.php
+++ templates/template-archive.php
@@ -5,55 +5,44 @@
 
 global $wpdb;
 
-$sql = "SELECT DISTINCT MONTH( post_date ) AS month, YEAR( post_date ) AS year, COUNT( id ) AS count
+$sql = "SELECT ID, post_title, post_date
         FROM $wpdb->posts
         WHERE post_status = 'publish'
           AND post_date <= now()
           AND post_type = 'post'
-        GROUP BY month, year
         ORDER BY post_date DESC";
-$months = $wpdb->get_results($sql);
+$archives = $wpdb->get_results($sql, ARRAY_A);
+$tmp_year = 0;
 ?>
-<?php while (have_posts()) : the_post(); ?>
 <article class="entry mdl-cell mdl-cell--12-col">
   <header>
     <h1 class="entry-title"><?php the_title(); ?></h1>
   </header>
   <div class="entry-content">
     <section>
-      <?php
-        $tmp_year = null;
-        foreach ($months as $row):
-          $year = $row->year;
-          $month = $row->month;
-
-          if ($tmp_year != $year) {
-            // ループの最初以外
-            if ($month !== reset($months)) {
-              echo '</ul>';  // 前のループのタグを閉じる
-            }
-
-            echo '<h2>'. $year .'</h2>';
-            echo '<ul class="archive-list">';
-        }
-        ?>
-        <li>
-          <a href="<?= BLOG_URL ?><?= $year; ?>/<?= str_pad($month, 2, 0, STR_PAD_LEFT); ?>">
-            <?= jdmonthname($month, 1); ?> (<?= $row->count; ?>)
-          </a>
-        </li>
-        <?php
-            // 次のループに渡す
-            $tmp_year = $year;
-
-            // ループが最後のとき
-            if ($month === end($months)) {
-              echo "</ul>"; // タグを閉じる
-            }
-
-          endforeach;
-        ?>
+    <?php foreach($archives as $entry): ?>
+    <?php
+      // set year
+      $current_year = intval( strtok($entry['post_date'], '-') );
+      if ($tmp_year != $current_year) {
+        echo "</ul>";  // for prev loop's
+        $tmp_year = $current_year;
+        echo "<h2>". $tmp_year ."</h2>";
+        echo "<ul class='archive-list'>";
+      }
+      $post_date = strtotime($entry['post_date']);
+    ?>
+    <li>
+      <time datetime="<?= date(DATE_W3C, $post_date) ?>"><?= date('M d', $post_date) ?></time>
+      <a href="<?= get_permalink($entry['ID']) ?>"><?= esc_html($entry['post_title']) ?></a>
+    </li>
+    <?php
+    // loop end
+    if ($entry === end($archives)) {
+      echo "</ul>";
+    }
+    ?>
+    <?php endforeach; ?>
     </section>
   </div>
 </article>
-<?php endwhile; ?>
