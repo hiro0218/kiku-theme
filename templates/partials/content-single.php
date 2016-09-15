b904dbe674cae369de0bbd6f955bf918c2ea7f99
hiro0218 <hiro0218@gmail.com>
2016-09-16 02:02:58 +0900

    enable mdl-list

--- templates/partials/content-single.php
+++ templates/partials/content-single.php
@@ -31,9 +31,9 @@
       if( !empty($similars) ): ?>
     <section class="entry-similar">
       <h2><?php _e('Related Articles', 'kiku'); ?></h2>
-      <ul>
+      <ul class='mdl-list'>
       <?php foreach($similars as $similar): ?>
-        <li><a href="<?= $similar['uri']; ?>"><?= $similar['title']; ?></a></li>
+        <li class="mdl-list__item"><a href="<?= $similar['uri']; ?>"><?= $similar['title']; ?></a></li>
       <?php endforeach; ?>
       </ul>
     </section>
