33a2050d4adeeeb799beb5b5e5bf644646b0199f
hiro0218 <hiro0218@gmail.com>
2016-09-24 22:32:32 +0900

    🗑 remove comment

--- templates/partials/content.php
+++ templates/partials/content.php
@@ -27,26 +27,3 @@
   </a>
   <?php get_template_part('partials/entry/schema'); ?>
 </article>
-<?php /*
-<article class="card-container mdl-cell mdl-cell--4-col mdl-card mdl-color--white" title="<?php the_title(); ?>" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
-  <a class="entry-wrap" href="<?php the_permalink(); ?>" itemprop="url">
-    <div class="entry-image">
-      <div class="entry-image-sheet" style="background-image: url('<?= $image_src ?>')">
-        <?php if ( empty($image_src) ): ?>
-        <div class="entry-image-none"><i class="material-icons">photo_camera</i></div>
-        <?php endif; ?>
-      </div>
-    </div>
-    <header class="mdl-card__title">
-      <h2 class="entry-title mdl-card__title-text"><?php the_title(); ?></h2>
-    </header>
-    <div class="entry-summary mdl-card__supporting-text mdl-card--expand">
-      <?php the_excerpt(); ?>
-    </div>
-    <footer class="mdl-card__supporting-text">
-      <?php get_template_part('partials/entry-meta'); ?>
-    </footer>
-  </a>
-  <?php get_template_part('partials/entry/schema'); ?>
-</article>
-*/
