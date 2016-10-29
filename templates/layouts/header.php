95b199b62a8e381cdfcb0679c2558db47036c71e
hiro0218 <hiro0218@gmail.com>
2016-10-30 01:44:58 +0900

    fix: header style

--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -1,15 +1,12 @@
 <header class="header-navigation mdl-layout__header mdl-layout__header--scroll">
   <div class="mdl-layout__header-row">
-    <span class="mdl-layout-title"><!-- Title -->
+    <span class="mdl-layout-title">
       <a class="brand" href="<?= BLOG_URL; ?>"><?php bloginfo('name'); ?></a>
     </span>
     <div class="mdl-layout-spacer"></div>
-    <!-- Search -->
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
-      <form method="get" class="header-search" action="<?= BLOG_URL; ?>">
-        <label class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer">
-          <i class="material-icons">search</i>
-        </label>
+    <div class="header-search mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+      <form method="get" action="<?= BLOG_URL; ?>">
+        <label class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer"><i class="material-icons">search</i></label>
         <div class="mdl-textfield__expandable-holder">
           <input class="mdl-textfield__input" type="search" value="<?= get_search_query(); ?>" name="s" id="fixed-header-drawer" required>
         </div>
