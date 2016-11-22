b0814bca357fd76abeb480712c0807bc217bf007
hiro <hiro0218@gmail.com>
2016-11-22 11:42:42 +0900

    merge: Sage 9-alpha.4

--- assets/build/webpack.config.watch.js
+++ assets/build/webpack.config.watch.js
@@ -5,8 +5,8 @@ const config = require('./config');
 
 module.exports = {
   output: { pathinfo: true },
-  debug: true,
   devtool: '#cheap-module-source-map',
+  stats: false,
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
@@ -15,7 +15,7 @@ module.exports = {
       target: config.devUrl,
       publicPath: config.publicPath,
       proxyUrl: config.proxyUrl,
-      browserSyncOptions: { files: config.watch },
+      watch: config.watch,
     }),
   ],
 };
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,7 +12,9 @@ import './vendor/mdl.js';
 // Use this variable to set up the common and page specific functions. If you
 // rename this variable, you will also need to rename the namespace below.
 const routes = {
+  // All pages
   common,
+  // Home page
   home,
   archive,
   search,
