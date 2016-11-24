d5d043c3d6ad87620ba27fcbdbb2c7e52524926b
hiro <hiro0218@gmail.com>
2016-11-24 20:18:44 +0900

    Revert "fix: mdl-ext loading modules"
    This reverts commit fedc2676b88791b0865c6f7bc615d400c74014a4.

--- assets/scripts/vendor/mdl.js
+++ assets/scripts/vendor/mdl.js
@@ -6,10 +6,4 @@ import 'material-design-lite/src/menu/menu';
 import 'material-design-lite/src/textfield/textfield';
 
 // mdl-ext
-import 'mdl-ext/src/utils/dom-utils';
-import 'mdl-ext/src/utils/string-utils';
-import 'mdl-ext/src/utils/json-utils';
-import 'mdl-ext/src/utils/full-throttle';
-import 'mdl-ext/src/utils/easing';
-import 'mdl-ext/src/utils/interval-function';
-import 'mdl-ext/src/sticky-header/sticky-header';
+import 'mdl-ext';
