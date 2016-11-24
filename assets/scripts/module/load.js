ae96a6e285471209590ce870bf2cf59fa2caf146
hiro <hiro0218@gmail.com>
2016-11-24 20:23:42 +0900

    fix: move anchor position logic

--- assets/scripts/module/load.js
+++ assets/scripts/module/load.js
@@ -18,13 +18,24 @@ module.exports = {
   compleatedLoading() {
     return new Promise((resolve, reject) => {
       var loader = document.getElementsByClassName('loader')[0];
-      loader.classList.add('is-loaded');
-      resolve(true);
+      if (loader) {
+        loader.classList.add('is-loaded');
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     });
   },
   moveAnchorTagPosition() {
-    var anchor = this.removeFristSharp(window.location.hash);
-    if (anchor) {
+    var hash = this.removeFristSharp(window.location.hash);
+    if (!hash) {
+      return;
+    }
+    var content = document.getElementsByClassName('mdl-layout__content')[0];
+    var anchor = document.getElementById(hash);
+    if (content && anchor) {
+      content.scrollTop = anchor.offsetTop;
+    } else {
       window.location.hash = '';
       window.location.hash = anchor;
     }
