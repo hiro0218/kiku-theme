6244e46eae0dddce681697de857a9ee04c0f5aae
hiro <hiro0218@gmail.com>
2016-12-08 15:50:40 +0900

    fix: condition of sitebar display

--- src/filters.php
+++ src/filters.php
@@ -10,12 +10,13 @@ use Roots\Sage\Template\Wrapper;
  * @link https://codex.wordpress.org/Conditional_Tags
  */
 add_filter('sage/display_sidebar', function ($display) {
+    return $display;
     // The sidebar will NOT be displayed if ANY of the following return true
-    return $display ? !in_array(true, [
-        // is_404(),
-        // is_front_page(),
-        is_page_template('templates/template-custom.php'),
-    ]) : $display;
+    // return $display ? !in_array(true, [
+    //     is_404(),
+    //     is_front_page(),
+    //     is_page_template('templates/template-custom.php'),
+    // ]) : $display;
 });
 
 /**
