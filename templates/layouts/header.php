491bcdb4b5ca6a131f2b3b6fb414dae55b6ed424
hiro0218 <hiro0218@gmail.com>
2017-01-13 01:05:24 +0900

    fix: ヘッダーの検索ボックスのレイアウト調整
    #3

--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -4,14 +4,17 @@
       <a class="header-title" href="<?= BLOG_URL; ?>"><?php bloginfo('name'); ?></a>
     </span>
     <div class="mdl-layout-spacer"></div>
-    <div class="header-search mdl-textfield mdl-js-textfield mdl-textfield--expandable">
-      <form method="get" action="<?= BLOG_URL; ?>">
-        <label class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer"><i class="material-icons">search</i></label>
-        <div class="mdl-textfield__expandable-holder">
-          <input class="mdl-textfield__input" type="search" value="<?= get_search_query(); ?>" placeholder="<?php _e('Search', 'kiku'); ?> <?= bloginfo('name') ?>" name="s" id="fixed-header-drawer" required>
-        </div>
-      </form>
-    </div>
+    <form class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+                 mdl-textfield--floating-label mdl-textfield--align-right"
+          method="get" action="<?= BLOG_URL; ?>">
+      <label class="mdl-button mdl-js-button mdl-button--icon"
+             for="fixed-header-drawer"><i class="material-icons">search</i>
+      </label>
+      <div class="mdl-textfield__expandable-holder">
+        <input class="mdl-textfield__input" name="s" id="fixed-header-drawer"
+               type="search" value="<?= get_search_query(); ?>" placeholder="<?php _e('Search', 'kiku'); ?> <?= bloginfo('name') ?>" required>
+      </div>
+    </form>
   </div>
 </header>
 <div class="mdl-layout__drawer">
