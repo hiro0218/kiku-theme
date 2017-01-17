96e957fca14653b74b39ed8766a92098f150b959
hiro0218 <hiro0218@gmail.com>
2017-01-18 01:38:24 +0900

    fix: the_alert関数

--- src/lib/Kiku/components/alert.php
+++ src/lib/Kiku/components/alert.php
@@ -1,8 +1,8 @@
 <?php
 namespace Kiku\Components;
 
-function the_alert(string $alert_kind, string $text, string $domain) {
+function the_alert(string $alert_kind, string $text) {
     echo "<div class='alert $alert_kind'>";
-    _e($text, $domain);
+    echo $text;
     echo '</div>';
 }
--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,6 @@
 <?php if (!have_posts()) : ?>
 <div class="mdl-cell mdl-cell--12-col mdl-card mdl-color--transparent">
-  <?php Kiku\Components\the_alert('alert-warning', 'Sorry, no results were found.', 'kiku');  ?>
+  <?php Kiku\Components\the_alert('alert-warning', __('Sorry, no results were found.', 'kiku'));  ?>
 </div>
 <?php else: ?>
   <?php get_template_part('partials/content', 'home'); ?>
