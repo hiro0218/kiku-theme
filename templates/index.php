e88db3e98f3cb3f40d68dcad8039b7c8a15eb382
hiro0218 <hiro0218@gmail.com>
2017-01-18 01:15:11 +0900

    fix: コード改善

--- templates/index.php
+++ templates/index.php
@@ -3,8 +3,5 @@
   <?php Kiku\Components\the_alert('alert-warning', 'Sorry, no results were found.', 'kiku');  ?>
 </div>
 <?php else: ?>
-<div class="entry-home-container mdl-cell mdl-cell--12-col">
-  <?php get_template_part('components/page-header'); ?>
   <?php get_template_part('partials/content', 'home'); ?>
-</div>
 <?php endif; ?>
