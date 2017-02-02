1ed66be4b87ceed5f00780d2e1e69829d03d5262
hiro0218 <hiro0218@gmail.com>
2017-02-02 16:58:24 +0900

    fix: フッターレイアウト

--- templates/layouts/base.php
+++ templates/layouts/base.php
@@ -3,25 +3,23 @@
   <?php get_template_part('layouts/head'); ?>
   <body <?php body_class(); ?>>
     <?php get_template_part('components/loader'); ?>
-    <div class="">
-      <?php
-        do_action('get_header');
-        get_template_part('layouts/header');
-      ?>
-      <div class="wrap" role="document">
-        <main class="container">
-          <?php include App\template()->main(); ?>
-        </main>
-        <div class="container">
-        <?php Kiku\Components\the_pager(); ?>
-        <?php Kiku\Components\the_pagination(); ?>
-        </div>
-        <?php
-          do_action('get_footer');
-          get_template_part('layouts/footer');
-          wp_footer();
-        ?>
+    <?php
+      do_action('get_header');
+      get_template_part('layouts/header');
+    ?>
+    <div class="wrap" role="document">
+      <main class="container">
+        <?php include App\template()->main(); ?>
+      </main>
+      <div class="container">
+      <?php Kiku\Components\the_pager(); ?>
+      <?php Kiku\Components\the_pagination(); ?>
       </div>
     </div>
+    <?php
+      do_action('get_footer');
+      get_template_part('layouts/footer');
+      wp_footer();
+    ?>
   </body>
 </html>
--- templates/layouts/footer.php
+++ templates/layouts/footer.php
@@ -1,14 +1,16 @@
-<footer id="footer" class="footer-navigation">
-  <div class="">
-    <?php if (has_nav_menu('primary_navigation')): ?>
-      <nav><?php wp_nav_menu([
-        'container' => '',
-        'theme_location' => 'primary_navigation',
-        'menu_class' => ''
-      ]);?></nav>
-    <?php endif; ?>
-  </div>
-  <div class="">
-    <span>&copy; <?php echo Kiku\Util::get_copyright_year(); ?> <a href="<?= BLOG_URL; ?>"><?= BLOG_NAME; ?></a>.</span>
+<footer class="footer footer-navigation">
+  <div class="container columns">
+    <div class="footer-menu column">
+      <?php if (has_nav_menu('primary_navigation')): ?>
+        <nav><?php wp_nav_menu([
+          'container' => '',
+          'theme_location' => 'primary_navigation',
+          'menu_class' => ''
+        ]);?></nav>
+      <?php endif; ?>
+    </div>
+    <div class="footer-copytight column is-one-third">
+      <span>&copy; <?php echo Kiku\Util::get_copyright_year(); ?> <a href="<?= BLOG_URL; ?>"><?= BLOG_NAME; ?></a>.</span>
+    </div>
   </div>
 </footer>
