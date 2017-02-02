9f73b5e9eb6692cf73319168eb912ccda1eb6b73
hiro0218 <hiro0218@gmail.com>
2017-02-02 23:44:40 +0900

    fix: ヘッダーのレイアウト

--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -1,23 +1,9 @@
 <header class="header-navigation">
-  <div class="">
-    <span class="">
-      <a class="header-title" href="<?= BLOG_URL; ?>"><?php bloginfo('name'); ?></a>
-    </span>
-    <div class=""></div>
-    <form class=""
-          method="get" action="<?= BLOG_URL; ?>">
-      <label class=""
-             for="fixed-header-drawer"><i class="material-icons">search</i>
-      </label>
-      <div class="">
-        <input class="" name="s" id="fixed-header-drawer"
-               type="search" value="<?= get_search_query(); ?>" placeholder="<?php _e('Search', 'kiku'); ?> <?= bloginfo('name') ?>" required>
-      </div>
-    </form>
+  <div class="container">
+    <a class="header-title" href="<?= BLOG_URL; ?>"><?php bloginfo('name'); ?></a>
   </div>
 </header>
-<div class="">
+
 <?php if (App\display_sidebar()) : ?>
   <?php get_template_part('layouts/sidebar'); ?>
 <?php endif; ?>
-</div>
