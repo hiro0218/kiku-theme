8b8445d37b0f3a2eb60e85ac8c3666e8dd060b3d
hiro0218 <hiro0218@gmail.com>
2017-02-05 01:37:58 +0900

    fix: フッターのレイアウト

--- templates/layouts/footer.php
+++ templates/layouts/footer.php
@@ -1,16 +1,18 @@
 <footer class="footer footer-navigation">
-  <div class="container columns">
-    <div class="footer-menu column">
-      <?php if (has_nav_menu('primary_navigation')): ?>
-        <nav><?php wp_nav_menu([
-          'container' => '',
-          'theme_location' => 'primary_navigation',
-          'menu_class' => ''
-        ]);?></nav>
-      <?php endif; ?>
-    </div>
-    <div class="footer-copytight column is-one-third">
-      <span>&copy; <?php echo Kiku\Util::get_copyright_year(); ?> <a href="<?= BLOG_URL; ?>"><?= BLOG_NAME; ?></a>.</span>
+  <div class="container">
+    <div class="columns is-mobile is-gapless">
+      <div class="footer-menu column">
+        <?php if (has_nav_menu('primary_navigation')): ?>
+          <nav><?php wp_nav_menu([
+            'container' => '',
+            'theme_location' => 'primary_navigation',
+            'menu_class' => ''
+          ]);?></nav>
+        <?php endif; ?>
+      </div>
+      <div class="footer-copytight column is-half">
+        <span>&copy; <?php echo Kiku\Util::get_copyright_year(); ?> <a href="<?= BLOG_URL; ?>"><?= BLOG_NAME; ?></a>.</span>
+      </div>
     </div>
   </div>
 </footer>
