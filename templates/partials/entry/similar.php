c5846f57c55b704f7d8df9d239f6a958f26d33bc
hiro0218 <hiro0218@gmail.com>
2017-02-05 22:28:42 +0900

    fix: 関連記事のレイアウト

--- templates/partials/entry/similar.php
+++ templates/partials/entry/similar.php
@@ -3,12 +3,14 @@
   $similars = $Entry->get_similar_posts(4);
 ?>
 <?php if( !empty($similars) ): ?>
-<section class="entry-similar columns is-multiline is-gapless">
-  <?php foreach($similars as $similar): ?>
-  <a href="<?= $similar['uri']; ?>" class="column is-half">
-    <div class="similar-title"><?= $similar['title']; ?></div>
-    <div class="similar-description"><?= $similar['description']; ?></div>
-  </a>
-  <?php endforeach; ?>
+<section class="entry-similar columns is-multiline">
+<?php foreach($similars as $similar): ?>
+  <div class="smilar-container column is-half">
+    <a href="<?= $similar['uri']; ?>">
+      <span class="similar-title"><?= $similar['title']; ?></span>
+      <span class="similar-description"><?= $similar['description']; ?></span>
+    </a>
+  </div>
+<?php endforeach; ?>
 </section>
 <?php endif; ?>
