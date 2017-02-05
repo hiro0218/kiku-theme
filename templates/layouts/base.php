f839ff08391195f86f6f77db622d9c30c478061e
hiro0218 <hiro0218@gmail.com>
2017-02-05 18:19:56 +0900

    fix: indent

--- templates/layouts/base.php
+++ templates/layouts/base.php
@@ -2,20 +2,20 @@
 <html <?php language_attributes(); ?>>
   <?php get_template_part('layouts/head'); ?>
   <body <?php body_class(); ?>>
-    <?php get_template_part('components/loader'); ?>
-    <?php
-      do_action('get_header');
-      get_template_part('layouts/header');
-    ?>
+  <?php get_template_part('components/loader'); ?>
+  <?php
+    do_action('get_header');
+    get_template_part('layouts/header');
+  ?>
     <main class="container">
     <?php include App\template()->main(); ?>
     <?php Kiku\Components\the_pager(); ?>
     <?php Kiku\Components\the_pagination(); ?>
     </main>
-    <?php
-      do_action('get_footer');
-      get_template_part('layouts/footer');
-      wp_footer();
-    ?>
+  <?php
+    do_action('get_footer');
+    get_template_part('layouts/footer');
+    wp_footer();
+  ?>
   </body>
 </html>
--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -1,17 +1,17 @@
 <header class="header-navigation nav">
-    <div class="header-title nav-left">
-      <a class="nav-item" href="<?= BLOG_URL; ?>"><?php bloginfo('name'); ?></a>
+  <div class="header-title nav-left">
+    <a class="nav-item" href="<?= BLOG_URL; ?>"><?php bloginfo('name'); ?></a>
+  </div>
+  <?php if (App\display_sidebar()) : ?>
+  <div class="header-menu nav-center">
+    <div class="nav-item">
+      <input id="drawer-trigger" class="drawer-checkbox" type="checkbox">
+      <label for="drawer-trigger" class="drawer-trigger">
+        <i class="material-icons">menu</i>
+      </label>
     </div>
-    <?php if (App\display_sidebar()) : ?>
-    <div class="header-menu nav-center">
-      <div class="nav-item">
-        <input id="drawer-trigger" class="drawer-checkbox" type="checkbox">
-        <label for="drawer-trigger" class="drawer-trigger">
-          <i class="material-icons">menu</i>
-        </label>
-      </div>
-    </div>
-    <?php endif; ?>
+  </div>
+  <?php endif; ?>
 </header>
 
 <label for="drawer-trigger" class="drawer-overlay"></label>
