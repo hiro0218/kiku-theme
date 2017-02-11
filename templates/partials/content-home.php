7922aab418a3acd43b63cdecdfa01390c4f0c516
hiro0218 <hiro0218@gmail.com>
2017-02-12 04:45:23 +0900

    fix: ホーム画面のリファクタ

--- templates/partials/content-home.php
+++ templates/partials/content-home.php
@@ -1,7 +1,7 @@
-<div class="entry-home-container">
-  <?php get_template_part('components/page-header'); ?>
+<?php get_template_part('components/page-header'); ?>
+<div class="entry-home">
   <?php while (have_posts()) : the_post(); ?>
-  <article class="entry-home" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+  <article class="entry-home-container" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
     <a href="<?php the_permalink(); ?>" itemprop="url">
       <?php get_template_part('partials/entry/thumbnail'); ?>
       <div class="entry-home-body">
@@ -9,7 +9,7 @@
           <h2 class="entry-home-title"><?php the_title(); ?></h2>
         </header>
         <div class="entry-home-summary"><?php the_excerpt(); ?></div>
-        <footer class="entry-home-footer">
+        <footer>
           <?php get_template_part('partials/entry/meta'); ?>
         </footer>
       </div>
