bdedee574a5c3431817ac0b571d9c52fb22b6ee2
hiro0218 <hiro0218@gmail.com>
2017-02-28 00:07:15 +0900

    add: アマゾンURLの正規化処理

--- src/lib/Kiku/Amazon.php
+++ src/lib/Kiku/Amazon.php
@@ -28,6 +28,7 @@ class Amazon {
 
         if (!empty($product_data)) {
             $product_data = json_encode($product_data, JSON_UNESCAPED_UNICODE);
+            $product_data = self::replace_detailPageUrl($product_data);
             self::update_asin_meta($post_id, $product_data);
         }
     }
@@ -78,6 +79,60 @@ class Amazon {
 
     public static function replace_amazon_image_scheme($image_url) {
         return str_replace('http://ecx.', 'https://images-na.ssl-', $image_url);
+    }
+
+    public static function replace_detailPageUrl($product_data) {
+        $product_data = json_decode($product_data, true);
+
+        $url = self::normalize_amazon_url($product_data['ASIN'], get_option('kiku_amazon_associate_tag'));
+        if (!empty($url)) {
+            $product_data["DetailPageURL"] = $url;
+        }
+
+        $product_data = json_encode($product_data, JSON_UNESCAPED_UNICODE);
+
+        return $product_data;
+    }
+
+    public static function normalize_amazon_url($asin, $associate = null) {
+        $domain = self::get_amazon_domain();
+
+        $url = 'https://'. $domain .'/exec/obidos/ASIN/'.$asin.'/';
+
+        if (!empty($associate)) {
+            $url .= $associate;
+        }
+
+        return $url;
+    }
+
+    private static function get_amazon_domain() {
+        $domain = '';
+
+        switch (get_locale()) {
+          case 'ja':
+          $domain = 'amazon.co.jp';
+          break;
+          case 'zh_CN':
+          $domain = 'amazon.cn';
+          break;
+          case 'de_DE':
+          $domain = 'amazon.de';
+          break;
+          case 'fr_FR':
+          $domain = 'amazon.fr';
+          break;
+          case 'it_IT':
+          $domain = 'amazon.it';
+          break;
+          case 'en_GB':
+          $domain = 'amazon.co.uk';
+          break;
+          default:
+          $domain = 'amazon.com';
+        }
+
+        return $domain;
     }
 
     public static function update_asin_meta($post_id, $value) {
