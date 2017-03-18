4a1e59ca3bbd9d61e2783abc203edbe44858dd6d
hiro0218 <hiro0218@gmail.com>
2017-03-19 02:56:50 +0900

    mod: サイドバー表示時に検索ボックスにフォーカス

--- assets/scripts/module/drawer.js
+++ assets/scripts/module/drawer.js
@@ -3,15 +3,23 @@ module.exports = {
   init() {
     var trigger = document.getElementsByClassName('drawer-checkbox')[0];
     if (trigger) {
+      var self = this;
       var body = document.body;
       var className = 'open-drawer';
       trigger.addEventListener('change', function (e) {
         if (trigger.checked) {
           body.classList.add(className);
+          self.focusSearchInput();
         } else {
           body.classList.remove(className);
         }
       });
+    }
+  },
+  focusSearchInput() {
+    var search = document.getElementById('widget_searchform');
+    if (search) {
+      search.focus();
     }
   }
 };
