950eac016baf8dfd1a041a670f9d57f4b655526d
hiro0218 <hiro0218@gmail.com>
2017-03-20 01:03:27 +0900

    fix: entry-metaのアイコン表示を修正

--- templates/partials/entry/breadcrumb.php
+++ templates/partials/entry/breadcrumb.php
@@ -2,12 +2,10 @@
 <nav class="breadcrumb">
 <ol itemscope itemtype="http://schema.org/BreadcrumbList">
   <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-    <i class="material-icons">home</i>
     <a href="<?= BLOG_URL ?>" itemprop="item">
       <span itemprop="name"><?= BLOG_NAME ?></span>
     </a>
   </li>
-  <li class="list-icon-arrow"><i class="material-icons">navigate_next</i></li>
   <?php if ( is_singular() ): ?>
   <?php
     $category = get_the_category();
@@ -28,13 +26,11 @@
   <?php foreach ( $allcats as $catid ): ?>
   <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
     <a href="<?= get_category_link($catid); ?>" itemprop="item"><span itemprop="name"><?= get_cat_name($catid); ?></span></a>
-    <i class="material-icons">navigate_next</i>
   </li>
   <?php endforeach; ?>
   <?php endif;  // $category is not empty ?>
   <?php endif;  // is_singular() ?>
   <li class="breadcrumb-active" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-    <i class="material-icons">location_on</i>
     <a href="<?= get_the_permalink(); ?>" itemprop="item">
       <span itemprop="name"><?php the_title_attribute(); ?></span>
     </a>
