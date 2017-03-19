c14e93393a211e47525965338d94a46555573efe
hiro0218 <hiro0218@gmail.com>
2017-03-19 22:45:37 +0900

    fix: スタイルの微調整

--- templates/partials/content-home.php
+++ templates/partials/content-home.php
@@ -1,14 +1,14 @@
 <?php get_template_part('components/page-header'); ?>
 <div class="entry-home">
   <?php while (have_posts()) : the_post(); ?>
-  <article class="entry-home-container" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+  <article class="entry-container" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
     <a href="<?php the_permalink(); ?>" itemprop="url">
       <?php get_template_part('partials/entry/thumbnail'); ?>
-      <div class="entry-home-body">
+      <div class="entry-body">
         <header>
-          <h2 class="entry-home-title"><?= esc_html(get_the_title()); ?></h2>
+          <h2 class="entry-title"><?= esc_html(get_the_title()); ?></h2>
         </header>
-        <div class="entry-home-summary"><?php the_excerpt(); ?></div>
+        <div class="entry-summary"><?php the_excerpt(); ?></div>
         <footer>
           <?php get_template_part('partials/entry/meta'); ?>
         </footer>
