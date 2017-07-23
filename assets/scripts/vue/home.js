826f4cbbcadcb6780f643b87b7b18752c5ea3e21
hiro0218 <hiro0218@gmail.com>
2017-07-23 23:14:05 +0900

    fix: 記事の並び順を更新順に変更

--- assets/scripts/vue/home.js
+++ assets/scripts/vue/home.js
@@ -34,7 +34,7 @@ module.exports = {
         },
         fetchPostData: function () {
           var self = this;
-          self.fetchAPI(`/wp-json/wp/v2/posts?per_page=${WP.per_page}&page=${WP.paged}`)
+          self.fetchAPI(`/wp-json/wp/v2/posts?per_page=${WP.per_page}&page=${WP.paged}&orderby=modified`)
           .then(function(jsons) {
             for (var key in jsons) {
               var json = jsons[key];
