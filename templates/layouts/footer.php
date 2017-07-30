1cd55be0b47a07334306b0f82ee3a257c9364abd
hiro0218 <hiro0218@gmail.com>
2017-07-31 00:42:13 +0900

    fix: footerレイアウト調整

--- templates/layouts/footer.php
+++ templates/layouts/footer.php
@@ -1,18 +1,16 @@
 <footer class="footer footer-navigation">
   <div class="container">
-    <div class="columns is-gapless">
-      <div class="footer-menu column">
-        <?php if (has_nav_menu('primary_navigation')): ?>
-          <nav><?php wp_nav_menu([
-            'container' => '',
-            'theme_location' => 'primary_navigation',
-            'menu_class' => ''
-          ]);?></nav>
-        <?php endif; ?>
-      </div>
-      <div class="footer-copytight column is-half">
-        <span>&copy; <?php echo Kiku\Util::get_copyright_year(); ?> <a href="<?= BLOG_URL; ?>"><?= BLOG_NAME; ?></a>.</span>
-      </div>
+    <div class="footer-menu">
+      <?php if (has_nav_menu('primary_navigation')): ?>
+        <nav><?php wp_nav_menu([
+          'container' => '',
+          'theme_location' => 'primary_navigation',
+          'menu_class' => ''
+        ]);?></nav>
+      <?php endif; ?>
+    </div>
+    <div class="footer-copytight">
+      <span>&copy; <?php echo Kiku\Util::get_copyright_year(); ?> <a href="<?= BLOG_URL; ?>"><?= BLOG_NAME; ?></a>.</span>
     </div>
   </div>
 </footer>
