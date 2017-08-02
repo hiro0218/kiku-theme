edc91c2189ac6f905a84de12dafe52ff452fff3b
hiro0218 <hiro0218@gmail.com>
2017-08-02 19:33:28 +0900

    fix: アイコンの位置調整

--- templates/partials/entry/breadcrumb.php
+++ templates/partials/entry/breadcrumb.php
@@ -4,15 +4,15 @@ $Schema->make_breadcrumb_list(); ?>
 <nav class="breadcrumb">
   <ol>
     <li>
+      <span class="icon-home"></span>
       <a href="<?= BLOG_URL ?>"><?= BLOG_NAME ?></a>
     </li>
     <li v-for="category in categories" v-cloak>
       <a v-bind:href="category.link">{{category.name}}</a>
     </li>
     <li class="breadcrumb-active">
-      <a href="<?= get_the_permalink(); ?>">
-        <span class="icon-location_on"></span><?php the_title_attribute(); ?>
-      </a>
+      <span class="icon-location_on"></span>
+      <a href="<?= get_the_permalink(); ?>"><?php the_title_attribute(); ?></a>
     </li>
   </ol>
 </nav>
