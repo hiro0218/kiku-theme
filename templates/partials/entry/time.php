f09cfb8c85c2de23d89013a34ae7fb4070607e46
hiro0218 <hiro0218@gmail.com>
2017-08-03 23:56:25 +0900

    fix: entry-metaレイアウト修正

--- templates/partials/entry/time.php
+++ templates/partials/entry/time.php
@@ -3,7 +3,7 @@
     <span class="icon-update"></span><time itemprop="datePublished" v-bind:datetime="date.publish">{{date.publish | formatDate}}</time>
   </li>
   <li class="date-modified" v-if="date.modified">
-    <span class="icon-update"></span><time itemprop="dateModified" v-bind:datetime="date.modified">{{date.modified | formatDate}}</time>
+    <time itemprop="dateModified" v-bind:datetime="date.modified">{{date.modified | formatDate}}</time>
   </li>
   <?php if (is_user_logged_in()): ?>
   <li><?php edit_post_link('<span class="icon-edit"></span>'); ?></li>
