d92fd4b84c9ee4ac3ec119398d792cc5c14c747f
hiro0218 <hiro0218@gmail.com>
2017-08-03 23:37:58 +0900

    fix: pagerレイアウト修正

--- templates/partials/entry/pager.php
+++ templates/partials/entry/pager.php
@@ -1,14 +1,14 @@
 <div class="pager" v-cloak v-if="pagers">
-  <div class="columns is-gapless">
-    <a v-if="pagers.prev" v-bind:href="pagers.prev.url" v-bind:title="pagers.prev.title" class="pager-previous column is-half">
-      <span class="pager-icon"><span class="icon-chevron_left"></span></span>
-      <span class="pager-instruct">prev</span>
-      <span class="pager-title">{{pagers.prev.title}}</span>
-    </a>
-    <a v-if="pagers.next" v-bind:href="pagers.next.url" v-bind:title="pagers.next.title" class="pager-next column is-half">
-      <span class="pager-icon"><span class="icon-chevron_right"></span></span>
-      <span class="pager-instruct">next</span>
-      <span class="pager-title">{{pagers.next.title}}</span>
-    </a>
-  </div>
+  <a v-if="pagers.prev" v-bind:href="pagers.prev.url" v-bind:title="pagers.prev.title" class="pager-prev">
+    <span class="pager-label">
+      <span class="pager-icon icon-chevron_left"></span>prev
+    </span>
+    <span class="pager-title">{{pagers.prev.title}}</span>
+  </a>
+  <a v-if="pagers.next" v-bind:href="pagers.next.url" v-bind:title="pagers.next.title" class="pager-next">
+    <span class="pager-label">
+      next<span class="pager-icon icon-chevron_right"></span>
+    </span>
+    <span class="pager-title">{{pagers.next.title}}</span>
+  </a>
 </div>
