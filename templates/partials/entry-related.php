0b454a2d069f086783eb4b3f87233a460b95cfc2
hiro0218 <hiro0218@gmail.com>
2017-08-06 01:52:45 +0900

    fix: related postsのレイアウト

--- templates/partials/entry-related.php
+++ templates/partials/entry-related.php
@@ -1,8 +1,11 @@
-<section class="entry-related">
-  <div class="related-container" v-for="(related,index) in relateds">
-    <a v-bind:href="related.uri">
-      <div class="related-title">{{ related.title }}</div>
-      <div class="related-description">{{ related.description }}</div>
-    </a>
+<section v-cloak v-if="relateds">
+  <h2 class="related-heading">Related Posts</h2>
+  <div class="entry-related">
+    <div class="related-container" v-for="(related,index) in relateds">
+      <a v-bind:href="related.uri">
+        <div class="related-title">{{ related.title }}</div>
+        <div class="related-description">{{ related.description }}</div>
+      </a>
+    </div>
   </div>
 </section>
