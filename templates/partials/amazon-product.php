a4cee1ccc09eba46037d360616a54469bd61268c
hiro0218 <hiro0218@gmail.com>
2017-08-08 23:20:15 +0900

    fix: AmazonProductのデザイン修正

--- templates/partials/amazon-product.php
+++ templates/partials/amazon-product.php
@@ -1,9 +1,9 @@
 <div class="amazon-product" v-cloak v-bind:style="{ 'background-image': 'url('+ amazon_product.LargeImage.URL +')' }" v-if="amazon_product">
-  <a v-bind:href="amazon_product.DetailPageURL" class="columns is-multiline is-vcentered" target="_blank">
-    <div class="column is-12">
+  <a v-bind:href="amazon_product.DetailPageURL" target="_blank">
+    <div class="product-image">
       <img v-bind:src="amazon_product.LargeImage.URL" data-zoom-disabled="true">
     </div>
-    <div class="column is-12">
+    <div class="product-title">
       <span class="amazon-title">{{amazon_product.ItemAttributes.Title}}</span>
     </div>
   </a>
