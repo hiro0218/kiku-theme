fd0b7ab92ae79959443c5997c6270c4cadf9fd79
hiro0218 <hiro0218@gmail.com>
2017-08-15 22:02:36 +0900

    fix: orderbyクエリを修正

--- assets/scripts/routes/Home.js
+++ assets/scripts/routes/Home.js
@@ -3,7 +3,7 @@ import home from '../vue/home';
 
 export default {
   init() {
-    home.view(`/wp-json/wp/v2/posts?per_page=${WP.per_page}&page=${WP.paged}&filter[orderby]=modified&categories_exclude=${WP.categories_exclude}`);
+    home.view(`/wp-json/wp/v2/posts?per_page=${WP.per_page}&page=${WP.paged}&orderby=modified&categories_exclude=${WP.categories_exclude}`);
   },
   finalize() {},
 };
