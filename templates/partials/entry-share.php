6da2e1bfce3789f97b14ee183286285bac78d65c
hiro0218 <hiro0218@gmail.com>
2017-08-15 11:37:12 +0900

    remove: translate()

--- templates/partials/entry-share.php
+++ templates/partials/entry-share.php
@@ -1,27 +1,27 @@
 <?php $Share = new \Share(); ?>
 <section class="entry-share">
 <?php if ($Share->display['twitter']): ?>
-  <a href="javascript:void(0)" class="btn-twitter" title="<?php __('Share on Twitter.', 'kiku'); ?>"
+  <a href="javascript:void(0)" class="btn-twitter" title="Share on Twitter"
     onClick="openWindow('//twitter.com/share?url=<?= $Share->meta['url']; ?>&text=<?= $Share->meta['title']; ?>',620,310); return false;">
     <span class="icon-twitter"></span>
   </a>
 <?php endif; ?>
 <?php if ($Share->display['facebook']): ?>
-  <a href="javascript:void(0)" class="btn-facebook" title="<?php __('Share on Facebook.', 'kiku'); ?>"
+  <a href="javascript:void(0)" class="btn-facebook" title="Share on Facebook"
     onClick="openWindow('//www.facebook.com/sharer/sharer.php?u=<?= $Share->meta['url']; ?>', 560,550); return false;">
     <span class="icon-facebook"></span>
   </a>
 <?php endif; ?>
 <?php if ($Share->display['hatena']): ?>
   <a href="//b.hatena.ne.jp/entry/<?= $Share->meta['url']; ?>" class="hatena-bookmark-button btn-hatena"
-     title="<?php __('Share on LINE.', 'kiku'); ?>"
+     title="Share on LINE"
      data-hatena-bookmark-title="<?= $Share->meta['title']; ?>"
      data-hatena-bookmark-layout="simple">
      <span class="icon-hatena"></span>
   </a>
 <?php endif; ?>
 <?php if ($Share->display['line']): ?>
-  <a href="javascript:void(0)" class="btn-line" title="<?php __('Share on LINE.', 'kiku'); ?>"
+  <a href="javascript:void(0)" class="btn-line" title="Share on LINE"
     onClick="openWindow('//lineit.line.me/share/ui?url=<?= $Share->meta['url']; ?>', 560,550); return false;">
     <span class="icon-line"></span>
   </a>
