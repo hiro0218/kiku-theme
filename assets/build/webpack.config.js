8360cead6726b2b3f95a598ba71fca4b3fe8f31f
hiro0218 <hiro0218@gmail.com>
2017-11-02 01:09:11 +0900

    fix: 不要なpostcssを整理

--- assets/build/webpack.config.js
+++ assets/build/webpack.config.js
@@ -85,12 +85,12 @@ let webpackConfig = {
             { loader: 'cache' },
             { loader: 'thread' },
             { loader: 'css', options: { sourceMap: config.enabled.sourceMaps } },
+            { loader: 'resolve-url', options: { sourceMap: config.enabled.sourceMaps } },
             { loader: 'postcss', options: {
                 config: { path: __dirname, ctx: config },
                 sourceMap: config.enabled.sourceMaps,
               },
             },
-            { loader: 'resolve-url', options: { sourceMap: config.enabled.sourceMaps } },
             { loader: 'sass', options: { sourceMap: config.enabled.sourceMaps } },
           ],
         }),
