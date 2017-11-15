012bb553bf8eb8e190193c6b066e40a802f7a1ea
hiro0218 <hiro0218@gmail.com>
2017-11-16 02:11:50 +0900

    fix: headroom offset/tolerance setting

--- assets/scripts/module/headroom.js
+++ assets/scripts/module/headroom.js
@@ -1,16 +1,13 @@
-import * as HeadroomJs from 'headroom.js/dist/headroom.js';
+import * as HeadroomJs from 'headroom.js/dist/headroom.min.js';
 import common from '@scripts/module/common';
 
 export default {
   init() {
     var headerNav = document.getElementsByClassName('header-navigation')[0];
     if (headerNav) {
-      var mainContainer = document.getElementsByClassName('main-container')[0];
-      var headroomOffset = common.getStyleSheetValue(mainContainer, 'padding-top') || 0;
-      headroomOffset = parseInt(headroomOffset, 10);
-
       var headroom = new HeadroomJs(headerNav, {
-        offset: headroomOffset
+        offset: 100,
+        tolerance: 25,
       });
 
       headroom.init();
