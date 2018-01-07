c9d12f6137f18871a1250dd38d476d9fa91e597e
hiro0218 <hiro0218@gmail.com>
2018-01-07 17:44:38 +0900

    update: package

--- assets/scripts/module/mokuji.js
+++ assets/scripts/module/mokuji.js
@@ -10,7 +10,6 @@ export default {
         anchorLinkSymbol: '#',
         anchorLinkBefore: false,
         anchorLinkClassName: 'anchor',
-        smoothScroll: true,
       });
       mokujiContent.appendChild(mokuji);
     }
