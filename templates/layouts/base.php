5eb203c6d42cdc1e5c509b03270423df2c9f4784
hiro0218 <hiro0218@gmail.com>
2018-01-19 11:09:51 +0900

    delete: layouts/main.php

--- templates/layouts/base.php
+++ templates/layouts/base.php
@@ -4,7 +4,9 @@
   <body <?php body_class(); ?>>
   <div id="app">
     <kiku-header></kiku-header>
-    <?php get_template_part('layouts/main');?>
+    <main class="main-container">
+      <?php include App\template()->main(); ?>
+    </main>
     <kiku-footer></kiku-footer>
   </div>
   <?php get_template_part('layouts/sidebar'); ?>
--- templates/layouts/main.php
+++
@@ -1,3 +0,0 @@
-<main class="main-container">
-  <?php include App\template()->main(); ?>
-</main>
