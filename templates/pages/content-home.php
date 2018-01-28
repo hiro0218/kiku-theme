a062781d45b6ee9919dd9c987a12fae1c32da398
hiro0218 <hiro0218@gmail.com>
2018-01-29 08:39:30 +0900

    fix: typo

--- templates/pages/content-home.php
+++ templates/pages/content-home.php
@@ -1,5 +1,5 @@
 <div class="container">
   <entry-list :total="headers.total" :page_title="page_title" :posts="posts"></entry-list>
   <home-content></home-content>
-  <entry-pagination :totalpages="headers.totalpages"></pagination>
+  <entry-pagination :totalpages="headers.totalpages"></entry-pagination>
 </div>
