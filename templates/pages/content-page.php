a8566898ab4259954c6a7469ea7d3baa0769da12
hiro0218 <hiro0218@gmail.com>
2018-02-07 00:21:54 +0900

    fix: content-singleの反映

--- templates/pages/content-page.php
+++ templates/pages/content-page.php
@@ -1,7 +1,6 @@
-<?php while (have_posts()) : the_post(); ?>
 <div class="container">
   <article class="entry">
-    <header>
+    <header class="entry-header">
       <h1 class="entry-title" v-html="$options.filters.escapeBrackets(title)"></h1>
       <div class="entry-meta">
         <entry-time :date="date"></entry-time>
@@ -9,9 +8,7 @@
       </div>
     </header>
     <section class="entry-content" v-html="content"></section>
-    <footer>
-      <?php get_template_part('partials/entry/breadcrumb'); ?>
-    </footer>
   </article>
 </div>
-<?php endwhile; ?>
+
+<entry-breadcrumb :title="title" :categories="categories"></entry-breadcrumb>
