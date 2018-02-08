0eb59f09cc8ce316b3cd1affdaf10bb962017a83
hiro0218 <hiro0218@gmail.com>
2018-02-08 18:56:38 +0900

    modify: drawerスタイル定義を移譲

--- assets/components/layout-sidebar.vue
+++ assets/components/layout-sidebar.vue
@@ -12,6 +12,7 @@ export default {
 </script>
 
 <style lang="scss">
+// sidebar
 .sidebar {
   padding: 0.5rem 0;
 
@@ -47,5 +48,65 @@ export default {
       padding-left: 3rem;
     }
   }
+}
+
+// drawer
+.drawer-trigger {
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.open-drawer {
+  overflow: hidden !important;
+
+  .drawer {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+
+  .drawer-overlay {
+    opacity: 1;
+    visibility: visible;
+  }
+}
+
+.drawer-checkbox {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 0;
+  height: 0;
+  opacity: 0;
+}
+
+.drawer {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  width: 256px;
+  height: 100%;
+  max-height: 100%;
+  transform: translate3d(100%, 0, 0);
+  transition: opacity 0.2s ease, transform 0.5s ease;
+  background: #fff;
+  opacity: 0;
+  z-index: 25;
+}
+
+.drawer-overlay {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transition: opacity 0.5s ease, visibility 0.2s ease;
+  background: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+  visibility: hidden;
+  z-index: 20;
 }
 </style>
