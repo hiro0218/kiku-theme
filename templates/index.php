f3afb798f24244cdc461e512515de3f2b4748804
hiro0218 <hiro0218@gmail.com>
2018-02-11 22:20:45 +0900

    fix: index.php

--- templates/index.php
+++ templates/index.php
@@ -1,9 +1,1 @@
-<?php if (have_posts()) : ?>
-  <entry-home></entry-home>
-<?php else : ?>
-  <div class="container">
-    <div class='alert alert-warning'>
-      <?php echo __('No results found.'); ?>
-    </div>
-  </div>
-<?php endif; ?>
+<entry-home></entry-home>
