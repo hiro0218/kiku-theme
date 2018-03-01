81f795eca9a340334177952294faac97363fb971
hiro0218 <hiro0218@gmail.com>
2018-03-01 21:38:53 +0900

    fix: term link

--- src/lib/Kiku/config/FrontVariables.php
+++ src/lib/Kiku/config/FrontVariables.php
@@ -51,16 +51,16 @@ class FrontVariables {
 
         // transient で一時的にキャッシュしたデータをロード
         $key = CACHE_PREFIX . "routes";
-        $routes = get_transient($key);
+        // $routes = get_transient($key);
 
-        if ($routes === false) {
+        // if ($routes === false) {
             $posts = $this->get_post_routes();
             $terms = $this->get_term_routes();
 
             $routes = array_merge($posts, $terms);
 
-            set_transient($key, $routes, MINUTE_IN_SECONDS * 15);
-        }
+            // set_transient($key, $routes, MINUTE_IN_SECONDS * 15);
+        // }
 
         return $routes;
     }
@@ -100,14 +100,11 @@ class FrontVariables {
         ]);
 
         foreach ($query->get_terms() as $term) {
-            $taxonomy = $term->taxonomy;
-            $name = $term->name;
-            $path = $taxonomy === 'category' ? '/category/' . $term->slug : '/tag/' . $term->slug;
             $routes[] = [
                 'id'       => $term->term_id,
-                'type'     => $taxonomy,
-                'path'     => $path,
-                'title'    => $name,
+                'type'     => $term->taxonomy,
+                'path'     => \Kiku\Util::base_path(get_term_link($term)),
+                'title'    => $term->name,
                 'slug'     => $term->slug,
                 'template' => '',
             ];
