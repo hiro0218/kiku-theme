3a37fd29f6e973034e246bf17d3bef8d95bdde4e
hiro0218 <hiro0218@gmail.com>
2018-03-05 09:54:55 +0900

    fix: スタイル定義を移動

--- assets/components/entry-tag.vue
+++ assets/components/entry-tag.vue
@@ -20,3 +20,35 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.entry-tag {
+  margin-bottom: 1rem;
+
+  ul {
+    margin-bottom: 0;
+    padding-left: 0;
+    font-size: $font-size-xs;
+    line-height: 2rem;
+    list-style: none;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 0.5rem 0.5rem 0;
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+
+  a {
+    padding: 0.5rem 0.7rem;
+    border-radius: $radius-sm;
+    background: $grey-100;
+    color: $grey-700;
+    &:hover {
+      background: $grey-200;
+    }
+  }
+}
+</style>
