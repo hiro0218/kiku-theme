82b97e9dfb1ab6a1617bd6de0deb5d35130b11cf
hiro0218 <hiro0218@gmail.com>
2018-03-10 17:30:25 +0900

    add: htmlentities function

--- assets/scripts/utils/index.js
+++ assets/scripts/utils/index.js
@@ -5,3 +5,20 @@ export const wait = (TIMER = 200) => {
     }, TIMER);
   });
 };
+
+export const htmlentities = {
+  encode(str) {
+    var buf = [];
+
+    for (let i = str.length - 1; i >= 0; i--) {
+      buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
+    }
+
+    return buf.join('');
+  },
+  decode(str) {
+    return str.replace(/&#(\d+);/g, function(match, dec) {
+      return String.fromCharCode(dec);
+    });
+  },
+};
