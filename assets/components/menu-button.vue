eb4299066129b31cc95d5262724c1b87b2169dff
hiro0218 <hiro0218@gmail.com>
2018-03-19 00:23:44 +0900

    fix: アイコンをsvg化

--- assets/components/menu-button.vue
+++ assets/components/menu-button.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="drawer-container">
     <input id="drawer-trigger" class="drawer-checkbox" type="checkbox" @click="toggeleDrawer">
-    <label for="drawer-trigger" class="drawer-trigger">
-      <span class="icon-menu"/>
-    </label>
+    <label for="drawer-trigger" class="drawer-trigger icon" v-html="iconMenu"/>
   </div>
 </template>
 
 <script>
+import iconMenu from '@/images/icon/menu.svg'
+
 export default {
   name: 'MenuButton',
+  data() {
+    return {
+      iconMenu,
+    };
+  },
   methods: {
     toggeleDrawer() {
       this.$store.commit('toggleSidebar');
@@ -17,3 +22,19 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.icon /deep/ {
+  display: inline-block;
+  vertical-align: middle;
+  text-align: center;
+  width: 1.5rem;
+  height: 1.5rem;
+  cursor: pointer;
+
+  svg {
+    display: block;
+    fill: $grey-700;
+  }
+}
+</style>
