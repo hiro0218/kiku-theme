b686d3c7e1719b1a247c084b4f50575a964acbe2
hiro0218 <hiro0218@gmail.com>
2018-03-23 00:01:31 +0900

    fix: pager layout

--- assets/components/entry-pager.vue
+++ assets/components/entry-pager.vue
@@ -1,19 +1,30 @@
 <template>
-  <div v-if="pager" class="pager">
-    <div class="pager-container">
-      <router-link v-if="pager.prev" :to="pager.prev.url" :title="pager.prev.title" class="pager-prev">
-        <span class="pager-label">previous</span>
-        <div class="pager-title">{{ pager.prev.title }}</div>
-      </router-link>
-      <router-link v-if="pager.next" :to="pager.next.url" :title="pager.next.title" class="pager-next">
-        <span class="pager-label">next</span>
-        <div class="pager-title">{{ pager.next.title }}</div>
-      </router-link>
-    </div>
+  <div v-if="pager" class="pager-container">
+    <router-link v-if="pager.prev" :to="pager.prev.url" :title="pager.prev.title" class="prev">
+      <div class="icon-container">
+        <div class="icon" v-html="iconArrowBack"/>
+      </div>
+      <div class="pager">
+        <span class="label">previous</span>
+        <div class="title">{{ pager.prev.title }}</div>
+      </div>
+    </router-link>
+    <router-link v-if="pager.next" :to="pager.next.url" :title="pager.next.title" class="next">
+      <div class="pager">
+        <span class="label">next</span>
+        <div class="title">{{ pager.next.title }}</div>
+      </div>
+      <div class="icon-container">
+        <div class="icon" v-html="iconArrowForward"/>
+      </div>
+    </router-link>
   </div>
 </template>
 
 <script>
+import iconArrowBack from '@/images/icon/arrow_back.svg?inline';
+import iconArrowForward from '@/images/icon/arrow_forward.svg?inline';
+
 export default {
   name: 'EntryPager',
   props: {
@@ -23,27 +34,38 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      iconArrowBack,
+      iconArrowForward,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.pager {
-  margin: 2rem 0;
+.icon /deep/ {
+  display: inline-block;
+  vertical-align: middle;
+  width: 1.5rem;
+
+  svg {
+    display: block;
+    fill: $grey-600;
+  }
 }
 
 .pager-container {
   display: flex;
+  margin: 2rem 0;
 
   a {
-    position: relative;
     display: flex;
-    flex: none;
-    flex-direction: column;
     width: 50%;
     padding: 1rem 0;
 
     &:hover {
-      .pager-title {
+      .title {
         color: $blue-300;
       }
     }
@@ -54,42 +76,45 @@ export default {
   }
 }
 
-.pager-label {
+.icon-container {
+  display: flex;
+  align-items: center;
+  width: 2rem;
+}
+
+.pager {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.label {
   margin-bottom: 0.25rem;
   color: $grey-400;
   font-weight: bold;
   text-transform: capitalize;
 }
 
-.pager-title {
+.title {
   color: $grey-600;
   transition: color 0.3s $animation-curve-fast-out-slow-in;
-  @include text-overflow;
 }
 
-.pager-prev {
+.prev {
   padding-right: 1rem;
   text-align: left;
-  &::before {
-    position: absolute;
-    right: 100%;
-    top: 25%;
-    margin: 1rem 0.75rem;
-    line-height: 1rem;
-    content: url('../images/icon/arrow_back.svg');
+
+  .icon-container {
+    margin-right: 0.5rem;
   }
 }
 
-.pager-next {
+.next {
   padding-left: 1rem;
   text-align: right;
-  &::after {
-    position: absolute;
-    left: 100%;
-    top: 25%;
-    margin: 1rem 0.75rem;
-    line-height: 1rem;
-    content: url('../images/icon/arrow_forward.svg');
+
+  .icon-container {
+    margin-left: 0.5rem;
   }
 }
 </style>
