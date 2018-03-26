90082e9a22e4502e5cdf9d7a0b7d2070672460cf
hiro0218 <hiro0218@gmail.com>
2018-03-26 17:27:15 +0900

    remove: console.log

--- src/scripts/utils/singular.js
+++ src/scripts/utils/singular.js
@@ -67,7 +67,6 @@ export default {
   setupSyntaxHighligh(entry) {
     const pre = entry.getElementsByTagName('pre');
     if (pre.length > 0) {
-      console.log('highlightAll');
       Prism.highlightAll();
     }
   },
